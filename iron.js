773ca8a2063f4d506ab43d3a6975c6f344e0055a
Zunaid5969 <74778429+Zunaid5969@users.noreply.github.com>
2021-06-08 19:29:34 +0530

    Add files via upload

---
+++ iron.js
@@ -0,0 +1,30 @@
+class iron{
+    constructor(x,y){
+        var options ={
+         restitution:0.8,
+         density:30,
+         friction:3
+        }
+       this.body=Bodies.rectangle(x,y,50,50,options);
+       this.width=50;
+       this.height=50;
+       World.add(world,this.body);
+    }
+    display(){
+     var p=this.body.position;
+     var ang=this.body.angle;
+
+     push();
+     translate(p.x,p.y);
+     rotate(angle);
+     strokeWeight(3);
+     stroke('white');
+     fill('red');
+    rect(0,0,this.width,this.height);
+     pop();
+    }
+}
+
+
+
+
